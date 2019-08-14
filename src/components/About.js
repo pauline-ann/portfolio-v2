7405272604fc508dd6bdf727cee8395b47889578
pauline-ann <paulinebantayan@gmail.com>
2019-08-13 19:42:35 -0700

    photo and contact card

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,61 @@
 import React from "react";
 import Navbar from "./Navbar";
+import { Container, Segment, Grid, Image, Header, Card, Tab, List } from "semantic-ui-react";
+import Me from "../assets/images/grad_me.jpg";
 
 const About = () => {
+
+  const panes = [
+    { menuItem: 'Intro', render: () => <Tab.Pane attached={false}>Tab 1 Content</Tab.Pane> },
+    { menuItem: 'Skills', render: () => <Tab.Pane attached={false}>Tab 2 Content</Tab.Pane> },
+    { menuItem: 'Hobbies', render: () => <Tab.Pane attached={false}>Tab 3 Content</Tab.Pane> },
+  ]
+
+  const listStyle = {
+    paddingLeft: "5%",
+    paddingBottom: "5%"
+  }
 
   return (
     <div>
       <Navbar />
-      <p>About</p>
-    </div>
+      <Container>
+        <Segment>
+          <Grid>
+            <Grid.Column width={5}>
+              <Card>
+                <Image src={Me} />
+                <Card.Content>
+                  <Card.Header>Pauline Ann Bantayan</Card.Header>
+                  <Card.Meta>UCSB Class of 2017</Card.Meta>
+                </Card.Content>
+                <Card.Description>
+                  <List style={listStyle}>
+                    <List.Item icon='marker' content='Los Angeles, CA' />
+                    <List.Item
+                      icon='mail'
+                      content={<a href='mailto:paulinebantayan@gmail.com'>paulinebantayan@gmail.com</a>}
+                    />
+                    <List.Item
+                      icon='phone volume'
+                      content="323-347-9057"
+                    />
+                  </List>
+                </Card.Description>
+              </Card>
+            </Grid.Column>
+            <Grid.Column width={7}>
+              <Container text>
+                <Header as="h2">
+                  About
+                </Header>
+                <Tab panes={panes} />
+              </Container>
+            </Grid.Column>
+          </Grid>
+        </Segment>
+      </Container>
+    </div >
   );
 }
 
--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Navbar from "./Navbar";
-import { Container, Image, Icon, Message, Divider } from "semantic-ui-react";
+import { Container, Image, Icon, Message, Divider, Segment } from "semantic-ui-react";
 import lobster from "../assets/images/lobster.png";
 import sushi from "../assets/images/sushi.png";
 import ramen from "../assets/images/ramen.png";
@@ -11,23 +11,25 @@ const Blog = () => {
     <div>
       <Navbar />
       <Container>
-        <Message color="yellow" size="huge">
-          <Message.Content>
-            <Divider hidden />
-            <Icon name='cogs' size="huge"/>
-            <Divider hidden />
-            <Message.Header>
-              Under Construction
+        <Segment textAlign="center">
+          <Message color="yellow" size="huge">
+            <Message.Content>
+              <Divider hidden />
+              <Icon name='cogs' size="huge" />
+              <Divider hidden />
+              <Message.Header>
+                Under Construction
             </Message.Header>
-            Will be delivering delicious content to you very soon!
+              Will be delivering delicious content to you very soon!
             <Image.Group size="small">
-              <Image src={ramen} />
-              <Image src={lobster} />
-              <Image src={sushi} />
-            </Image.Group>
-            <Divider hidden />
-          </Message.Content>
-        </Message>
+                <Image src={ramen} />
+                <Image src={lobster} />
+                <Image src={sushi} />
+              </Image.Group>
+              <Divider hidden />
+            </Message.Content>
+          </Message>
+        </Segment>
       </Container>
     </div>
   );
