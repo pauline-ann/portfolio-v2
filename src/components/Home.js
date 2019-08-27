00b34c0da9df734ec87bd9010566fad2bcd5e4c2
pauline-ann <paulinebantayan@gmail.com>
2019-08-27 14:03:46 -0700

    home image animation

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,12 @@
 import React from "react";
-import { Container, Header, Image, Button, Divider, Icon } from "semantic-ui-react";
+import { Container, Header, Button, Divider, Icon } from "semantic-ui-react";
 import { Link } from 'react-router-dom';
+import CSSTransition from 'react-transition-group/CSSTransition';
+import { Image, BackgroundImage } from 'react-image-and-background-image-fade';
+
+// Image
 import Avatar from "../assets/images/ny.jpeg";
+import Background from "../assets/images/desk.jpg"
 
 //Components
 import Navbar from "./Navbar";
@@ -32,17 +37,59 @@ const Home = () => {
   }
 
   const avatarStyle = {
-    marginTop: "7em"
+    marginTop: "7em",
+    width: "160px",
+    height: "160px",
+    borderRadius: "50%"
   }
 
   return (
-    <div className="Home">
-      <Navbar />
+    <div>
+      <BackgroundImage
+        useChild
+        transitionTime="0.2s"
+        src={Background}
+        className="Home"
+      >
+        <div className="Home">
+          <Navbar />
+          <Container
+            text
+            vertical
+            textAlign="center"
+            style={containerStyle}
+          >
+            <Image
+              src={Avatar}
+              style={avatarStyle}
+              transitionTime="1.8s"
+              alt="avatar"
+            />
+            <Header
+              as='h1'
+              content='Pauline Ann Bantayan'
+              style={headerStyle1}
+            />
+            <Header
+              as='h2'
+              content='Full-stack Developer & Full-time Eater from Los Angeles, CA.'
+              style={headerStyle2}
+            />
+            <Divider hidden />
+            <Button as={Link} to='/projects' basic color='violet'><Icon color="violet" name="globe" /> Projects</Button>
+            <Button as={Link} to='/blog' basic color='violet'><Icon color="violet" name="food" /> Blog</Button>
+
+          </Container>
+          <Footer page="Home" />
+        </div>
+      </BackgroundImage>
+      {/* <Navbar />
       <Container
         text
         vertical
         textAlign="center"
-        style={containerStyle}>
+        style={containerStyle}
+      >
         <Image src={Avatar} size="small" circular centered
           style={avatarStyle}
         />
@@ -61,7 +108,7 @@ const Home = () => {
         <Button as={Link} to='/blog' basic color='violet'><Icon color="violet" name="food" /> Blog</Button>
 
       </Container>
-      <Footer page="Home"/>
+      <Footer page="Home" /> */}
     </div>
   );
 }
