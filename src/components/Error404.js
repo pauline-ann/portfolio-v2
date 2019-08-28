f13db41a1a92f50bc6cec9765598f8703f24c686
pauline-ann <paulinebantayan@gmail.com>
2019-08-27 23:42:58 -0700

    add home button to error page

--- src/components/Error404.js
+++ src/components/Error404.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Container, Button, Icon } from "semantic-ui-react";
+import { Link } from 'react-router-dom';
 import Navbar from "./Navbar/Navbar";
 import Footer from "./Footer/Footer";
 
@@ -7,9 +9,23 @@ const Error404 = (props) => {
     return (
         <div>
             <Navbar />
-            <p style={{ "text-align": "center" }}>Sorry! Page not found for {props.location.pathname}.</p>
+            <Container textAlign="center" centered style={{ minHeight: "80vh", position: "relative", paddingTop: "15em" }}>
+                <p>Sorry! Page not found for {props.location.pathname}.</p>
+                <Button
+                    as={Link}
+                    to='/'
+                    basic
+                    color='violet'
+                >
+                    <Icon
+                        color="violet"
+                        name="home"
+                    />
+                    Back to home
+            </Button>
+            </Container>
             <Footer />
-        </div>
+        </div >
     );
 }
 
