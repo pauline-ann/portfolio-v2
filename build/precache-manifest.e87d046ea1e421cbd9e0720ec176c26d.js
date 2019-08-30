dedcec92029327bd11f3b481c311d1290b7e5fca
pauline-ann <paulinebantayan@gmail.com>
2019-08-29 18:58:12 -0700

    create backend server and build for deployment

---
+++ build/precache-manifest.e87d046ea1e421cbd9e0720ec176c26d.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aab9395ddb8a0a366681d4b9834aaf1a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f5df35b899bec8e5159e",
+    "url": "/static/css/2.e70aa027.chunk.css"
+  },
+  {
+    "revision": "de42e805de0e43b920f4",
+    "url": "/static/css/main.0af9dea6.chunk.css"
+  },
+  {
+    "revision": "f5df35b899bec8e5159e",
+    "url": "/static/js/2.54fcd2c1.chunk.js"
+  },
+  {
+    "revision": "de42e805de0e43b920f4",
+    "url": "/static/js/main.5d8c9f25.chunk.js"
+  },
+  {
+    "revision": "56bf258127c6ab1846d2",
+    "url": "/static/js/runtime~main.9f6b3058.js"
+  },
+  {
+    "revision": "d8096629e71502fc3f18d6b6f040f83d",
+    "url": "/static/media/PaulineBantayanResume.d8096629.pdf"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "8882f5d9457af480ad3d642dd0421327",
+    "url": "/static/media/desk.8882f5d9.jpg"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "60bb5c0e2970d14ff8c317c6317a9752",
+    "url": "/static/media/grad.60bb5c0e.jpg"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "25dc96027231c5792e4e670d7372677b",
+    "url": "/static/media/lighthouse_1.25dc9602.jpg"
+  },
+  {
+    "revision": "de1e0f66d7f1562d76e90c426908c07a",
+    "url": "/static/media/lighthouse_2.de1e0f66.jpg"
+  },
+  {
+    "revision": "25dc96027231c5792e4e670d7372677b",
+    "url": "/static/media/lighthouse_small.25dc9602.jpg"
+  },
+  {
+    "revision": "132330984c687bb495bceae9fd42ce67",
+    "url": "/static/media/mmh_1.13233098.jpg"
+  },
+  {
+    "revision": "1a6d97b4137753c5234843001c9d62e5",
+    "url": "/static/media/mmh_2.1a6d97b4.jpg"
+  },
+  {
+    "revision": "132330984c687bb495bceae9fd42ce67",
+    "url": "/static/media/mmh_small.13233098.jpg"
+  },
+  {
+    "revision": "f046fc8251c9f98a97924e495725eae9",
+    "url": "/static/media/ny.f046fc82.jpeg"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "9e6fb47d65fdfd5be681d8f0fecc90e7",
+    "url": "/static/media/pb_logo_grey.9e6fb47d.png"
+  },
+  {
+    "revision": "e443f6fb148cc36b22f1d9babba1f102",
+    "url": "/static/media/zephyr_1.e443f6fb.jpg"
+  },
+  {
+    "revision": "6b6bab66a946b004a21fb748880887c9",
+    "url": "/static/media/zephyr_2.6b6bab66.jpg"
+  },
+  {
+    "revision": "7ecaa21e3455e0eec0c5abf4970ba147",
+    "url": "/static/media/zephyr_small.7ecaa21e.jpg"
+  }
+]);
---
+++ server.js
@@ -0,0 +1,38 @@
+// ----- Reactfolio Server -/
+
+// ----- Dependencies -/
+const express = require('express');
+const favicon = require('express-favicon');
+const path = require('path');
+
+// ----- Setup -/
+
+// Server Port
+const port = process.env.PORT || 8080;
+const app = express();
+
+// Serve up static assets
+app.use(favicon(__dirname + '/build/favicon.ico'));
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
+
+// User parsers
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// ----- Mount Router -/
+
+app.get('/ping', function (req, res) {
+  return res.send('pong');
+});
+
+// Define any API routes before this runs
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+// ----- Listen
+app.listen(port, () => {
+  console.log("------------------------------------------------------------");
+  console.log(`React portfolio application running on port ${port}...`);
+});
