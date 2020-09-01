ec347c8aeea7cd8c00471f75e24047a72e1dd14d
pauline-ann <paulinebantayan@gmail.com>
2020-08-31 21:15:33 -0700

    add responsiveness for large desktops

--- src/components/Error404.js
+++ src/components/Error404.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import { Container, Button, Icon } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
-import Navbar from './Navbar/Navbar';
 
 const Error404 = (props) => {
 
     return (
         <div>
-            <Navbar />
             <Container textAlign='center' centered style={{ minHeight: '80vh', position: 'relative', paddingTop: '15em' }}>
                 <p>Sorry! Page not found for {props.location.pathname}.</p>
                 <Button
