b297b04e9f79163884008dc8ba3c9beba3d9a7ca
pauline-ann <paulinebantayan@gmail.com>
2022-12-15 14:41:59 -0800

    import pixel font

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import './index.css'
+import './fonts/m5x7.ttf'
 import 'semantic-ui-css/semantic.min.css'
 
 const container = document.getElementById('root')
